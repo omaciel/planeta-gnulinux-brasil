7c468dcb563f689b924189bbd713e69137cdcf1f
Og Maciel <omaciel@munster.ogmaciel.com>
2009-02-14 08:15:28 -0500

    Some more tweaks to the posts page and trying to get the author gotchi to show up.

--- planeta/views.py
+++ planeta/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+# Create your views here.
+from django.shortcuts import render_to_response
+
+from models import Post
+
+def post(request, post_id):
+    post = Post.objects.get(id=artigo_id)
+        return render_to_response('planeta/post.html', locals())
